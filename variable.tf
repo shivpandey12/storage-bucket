e38befff008d12c782b1ceaea3ef2bc79992970c
shivpandey12 <121170352+shivpandey12@users.noreply.github.com>
2024-07-12 18:19:58 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "GCP Project ID"
+}
+
+variable "region" {
+  description = "GCP region for bucket location"
+}
+
+variable "bucket_name" {
+  description = "Name of the GCP Storage Bucket"
+}
