a867d1b250cc40880fbb6ad4557b4776870256a6
shivpandey12 <121170352+shivpandey12@users.noreply.github.com>
2024-07-12 18:46:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-"access_token" = "1//0gL9g4a9e8Kg9CgYIARAAGBASNwF-L9IrmBBlbsP6JBBK1dOfpevOos3H5wh0Mkw9QFX9xAQHD8ed1UsZKXq9wzC2PP3luEdYcQ0"
+access_token = "1//0gL9g4a9e8Kg9CgYIARAAGBASNwF-L9IrmBBlbsP6JBBK1dOfpevOos3H5wh0Mkw9QFX9xAQHD8ed1UsZKXq9wzC2PP3luEdYcQ0"
   project     = var.project_id
   region      = var.region
 }
