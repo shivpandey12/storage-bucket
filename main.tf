c95f401b6abb057ecfa7f7877ae4fad020df1c98
shivpandey12 <121170352+shivpandey12@users.noreply.github.com>
2024-07-13 19:29:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
-resource "google_storage_bucket" "my-bucket" {
-  name          = var.bucket_name
-  location      = var.region
-  force_destroy = true
+resource "google_storage_bucket" "my_bucket" {
+  name     = var.bucket_name
+  location = var.bucket_location
+
+  versioning {
+    enabled = true
+  }
+
+  # Uncomment if you want to make the bucket publicly accessible
+  # uniform_bucket_level_access = true
+
+  # Uncomment if you want to create a lifecycle rule
+  # lifecycle_rule {
+  #   action {
+  #     type = "Delete"
+  #   }
+  #   condition {
+  #     age = 30
+  #   }
+  # }
 }
