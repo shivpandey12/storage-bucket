c9c9029b0a46885df755b50b2aa27a74cd8fbc5e
shivpandey12 <121170352+shivpandey12@users.noreply.github.com>
2024-07-24 10:39:25 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 
 provider "google" {
-  credentials = file("desktop_location/keys.json.json")
+  credentials = file("desktop_location/key.json")
   project     = var.project_id
   region      = var.region
 }
